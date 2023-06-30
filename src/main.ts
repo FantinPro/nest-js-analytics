27ccc48ec37e1dc18dacaab6379e09d24060e43a
horek <azenoxe@gmail.com>
2023-06-30 09:54:45 +0200

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,9 @@ import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
-  app.enableCors();
 
   await app.listen(3000);
 }
